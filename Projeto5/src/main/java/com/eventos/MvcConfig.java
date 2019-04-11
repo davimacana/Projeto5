68b0b3ea0172fc2766a348f49e17b6ab6288706a
davi.macana <macana.davi@gmail.com.br>
2019-04-11 17:23:11 -0300

    Projeto5.

--- Projeto5/src/main/java/com/eventos/MvcConfig.java
+++ Projeto5/src/main/java/com/eventos/MvcConfig.java
@@ -1,7 +1,6 @@
 package com.eventos;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,26 +9,11 @@ public class MvcConfig implements WebMvcConfigurer {
 
 	@Override
     public void addViewControllers(ViewControllerRegistry registry) {
-    	registry.addViewController("/teste").setViewName("teste");
-        registry.addViewController("/").setViewName("login");
+    	registry.addViewController("/").setViewName("login");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/dashboard").setViewName("dashboard");
         registry.addViewController("/typography").setViewName("typography");
         registry.addViewController("/icons").setViewName("icons");
-    }
-    
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    	registry.addResourceHandler(
-                "/webjars/**",
-                "/img/**",
-                "/css/**",
-                "/js/**")
-                .addResourceLocations(
-                        "classpath:/META-INF/resources/webjars/",
-                        "classpath:/static/img/",
-                        "classpath:/static/css/",
-                        "classpath:/static/js/");
     }
 
 }
